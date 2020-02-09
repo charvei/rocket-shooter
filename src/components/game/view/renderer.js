972c8bdb635fd534d06fa2730f122430e50b4be5
craig <craigharvei@gmail.com>
2020-02-09 14:04:20 +1100

    removed unused grid, added game js files to git ignore

--- src/components/game/view/renderer.js
+++ src/components/game/view/renderer.js
@@ -1,8 +1,8 @@
 "use strict";
 exports.__esModule = true;
-//Should change this into a RenderingManager class. This guy can hold the gameGrid and manage passing data between
+//Should change this into a RenderingManager class?
 var Renderer = /** @class */ (function () {
-    function Renderer(context, canvasProps, gameGrid, colours) {
+    function Renderer(context, canvasProps, colours) {
         var _this = this;
         // Maybe when this gets more complicated this can be made more intelligent?
         this.drawWorld = function (characterList, objectList) {
@@ -17,18 +17,6 @@ var Renderer = /** @class */ (function () {
         this.testDraw = function () {
             _this.context.fillStyle = "#000000";
             _this.context.fillRect(0, 0, _this.canvasProps.height, _this.canvasProps.width);
-        };
-        this.drawPixels = function () {
-            for (var x = 0; x < _this.canvasProps.width; x++) {
-                for (var y = 0; y < _this.canvasProps.height; y++) {
-                    try {
-                        _this.context.putImageData(_this.gameGrid.getCoord(x, y).getImageData(), x, y);
-                    }
-                    catch (error) {
-                        throw error;
-                    }
-                }
-            }
         };
         // So at some point may want to consider having multiple canvas or something right? buffer screens and stuff
         this.drawCharacters = function (characterList) {
@@ -45,7 +33,6 @@ var Renderer = /** @class */ (function () {
         };
         this.context = context;
         this.canvasProps = canvasProps;
-        this.gameGrid = gameGrid;
         this.colours = colours;
     }
     return Renderer;
--- src/components/game/view/renderer.ts
+++ src/components/game/view/renderer.ts
@@ -1,22 +1,19 @@
-import GameGrid from './grid.js'
 import Colours from '../world/colours.js'
 import Character from '../world/Character.js' //not sure if i should be importing character? i would like it to be even more decoupled right?
 import GameObject from '../world/objects/GameObject.js'
 
-//Should change this into a RenderingManager class. This guy can hold the gameGrid and manage passing data between
+//Should change this into a RenderingManager class?
 class Renderer {
     context: CanvasRenderingContext2D
     canvasProps: {
         height:number
         width:number
     }
-    gameGrid: GameGrid
     colours: Colours
 
-    constructor(context:CanvasRenderingContext2D, canvasProps: {height: number, width: number}, gameGrid:GameGrid, colours:Colours) {
+    constructor(context:CanvasRenderingContext2D, canvasProps: {height: number, width: number}, colours:Colours) {
         this.context = context
         this.canvasProps = canvasProps
-        this.gameGrid = gameGrid
         this.colours = colours
     }
 
@@ -38,18 +35,6 @@ class Renderer {
     testDraw = () => {
         this.context.fillStyle = "#000000" 
         this.context.fillRect(0, 0, this.canvasProps.height, this.canvasProps.width)
-    }
-
-    drawPixels = () => {
-        for (let x:number = 0; x<this.canvasProps.width; x++) {
-            for (let y:number = 0; y<this.canvasProps.height; y++) {
-                try {
-                    this.context.putImageData(this.gameGrid.getCoord(x, y).getImageData(), x, y)
-                } catch (error) {
-                    throw error
-                }
-            }
-        }
     }
 
     // So at some point may want to consider having multiple canvas or something right? buffer screens and stuff
